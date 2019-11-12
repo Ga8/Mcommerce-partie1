c543f5394e5562afbbcc6c685c1ded3304a9b13b
Ga8 <guibaud.gaetan@hotmail.fr>
2019-11-12 22:32:22 +0100

    ProduitGratuitException

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
@@ -1,4 +1,13 @@
 package com.ecommerce.microcommerce.web.exceptions;
 
-public class ProduitGratuitException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class ProduitGratuitException extends RuntimeException{
+
+    public ProduitGratuitException(String s){
+        super(s);
+    };
+
 }
